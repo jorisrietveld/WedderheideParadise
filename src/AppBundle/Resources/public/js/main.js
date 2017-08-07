fddf810e4172b77122137200509067bac3683116
Joris Rietveld <jorisrietveld@gmail.com>
2017-08-07 12:54:02 +0200

    Re organizing the template system and routing.

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -3,7 +3,7 @@
  * Created: 16-07-2017 13:57
  * Licence: GPLv3 - General Pulbic Licence version 3
  */
-
+/*
 (function ($) {
 	"use strict"; // Start of use strict
 
@@ -14,7 +14,7 @@
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				$('html, body').animate({
-					scrollTop: (target.offset().top + 20)
+					scrollTop: (target.offset().top - 60)
 				}, 1000, "easeInOutExpo");
 				return false;
 			}
@@ -34,52 +34,23 @@
 
 })(jQuery); // End of use strict
 
-/*
-var el = document.body; //Parent Element. Text is centered inside.
-var mainText = "THIS IS THE FIRST LINE"; //Header Text.
-var subText = "THIS TEXT HAS A KNOCKOUT EFFECT"; //Knockout Text.
-var fontF = "Roboto, Arial"; //Font to use.
-var mSize = 42; //Text size.
+var current_section = 1;
+$(window).mousewheel(function (event, delta) {
+	if (delta > 0) {
+		current_section = current_section - 1; //up
+		$('body, html').stop().animate({
+			scrollTop: $('a[name="section' + current_section + '"]').offset().top
+		}, 'slow');
+	} else if (delta < 0) {
+		current_section = current_section + 1; //down
+		$('body, html').stop().animate({
+			scrollTop: $('a[name="section' + current_section + '"]').offset().top
+		}, 'slow');
+	}
+	return false;
+});
 
-//Centered text display:
-var tBox = centeredDiv(el), txtMain = mkDiv(tBox, mainText), txtSub = mkDiv(tBox),
-	ts = tBox.style, stLen = textWidth(subText, fontF, mSize)+5; ts.color = "#fff";
-ts.font = mSize+"pt "+fontF; ts.fontWeight = 100; txtSub.style.fontWeight = 400;
-
-//Generate subtext SVG for knockout effect:
-txtSub.innerHTML =
-	"<svg xmlns='http://www.w3.org/2000/svg' width='"+stLen+"px' height='"+(mSize+11)+"px' viewBox='0 0 "+stLen+" "+(mSize+11)+"'>"+
-	"<rect x='0' y='0' width='100%' height='100%' fill='#fff' rx='4px' ry='4px' mask='url(#txtSubMask)'></rect>"+
-	"<mask id='txtSubMask'>"+
-	"<rect x='0' y='0' width='100%' height='100%' fill='#fff'></rect>"+
-	"<text x='"+(stLen/2)+"' y='"+(mSize+6)+"' font='"+mSize+"pt "+fontF+"' text-anchor='middle' fill='#000'>"+subText+"</text>"+
-	"</mask>"+
-	"</svg>";
-
-//Relevant Helper Functions:
-function centeredDiv(parent) {
-	//Container:
-	var d = document.createElement('div'), s = d.style;
-	s.display = "table"; s.position = "relative"; s.zIndex = 999;
-	s.top = s.left = 0; s.width = s.height = "100%";
-	//Content Box:
-	var k = document.createElement('div'), j = k.style;
-	j.display = "table-cell"; j.verticalAlign = "middle";
-	j.textAlign = "center"; d.appendChild(k);
-	parent.appendChild(d); return k;
-}
-
-function mkDiv(parent, tCont) {
-	var d = document.createElement('div');
-	if(tCont) d.textContent = tCont;
-	parent.appendChild(d); return d;
-}
-
-function textWidth(text, font, size) {
-	var canvas = window.textWidthCanvas || (window.textWidthCanvas = document.createElement("canvas")),
-		context = canvas.getContext("2d"); context.font = size+(typeof size=="string"?" ":"pt ")+font;
-	return context.measureText(text).width;
-
-}
-
-	*/
+$('body, html').scroll(function () {
+	return false;
+}); // allow no scrolling
+*/
